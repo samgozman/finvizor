451934c0f3cd755c8cdcad1f76b826c48d2c20ca
samgozman <sam@gozman.space>
2021-04-14 10:59:05 +0300

    Fix: Stocks with extension (like RDS.A)

--- __tests__/stock.test.js
+++ __tests__/stock.test.js
@@ -12,6 +12,15 @@ test('Should get stock response from finviz', async () => {
     expect(stock.insidersDeals.length).toBeGreaterThan(0)
 })
 
+test('Should get stock with extension (RDS.A) from finviz', async () => {
+    let stock = await finvizor.stock('RDS.A')
+    // Assert that response is not null at least
+    expect(stock).not.toEqual({})
+
+    // Assert that String data from the response is correct
+    expect(stock.ticker).toBe('RDS.A')
+})
+
 test('Should get error response from finviz', async () => {
     let stock = await finvizor.stock('Kek lol')
     // Assert that response is not null at least
